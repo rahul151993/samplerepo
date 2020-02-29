cd205708c22acb1a2dc0ecd0a7a4f32a4d06292f
rahulglp1993 <rahul@mazhars-MacBook-Air.local>
2020-02-29 19:43:15 +0530

    checking with jenkins

--- SampleGreeting.java
+++ SampleGreeting.java
@@ -1,5 +1,6 @@
 public class SampleGreeting{
 	public static void main(String args[]){
 		System.out.println("This is sample greeting don't take it seriously");
+		System.out.println("another for the testing purpose");
 	}
 }
